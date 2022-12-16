c067076f508ac6d55aabfda7e37023b46cfb1121
nahee-park <nahee.developer@gmail.com>
2022-12-17 06:54:37 +0900

    feat: add wrapPromise to throw promise status

--- example/src/components/CardList.tsx
+++ example/src/components/CardList.tsx
@@ -1,32 +1,37 @@
-import useNasaData from '../hooks/useNasaData';
+import { Suspense } from 'react';
+import { fetchNasaData } from '../lib/api';
 
 import EachCard from './Card';
+import ErrorBoundary from './common/ErrorBoundary';
 
 function CardList() {
-  const { loading, data, error } = useNasaData();
+  return (
+    <ErrorBoundary renderFallback={({ error, reset }) => <p>에러 났어요 {error?.message}</p>}>
+      <Suspense fallback={<p>로딩중 ...</p>}>
+        <Resolved resource={fetchNasaData('earth', 1)} />
+      </Suspense>
+    </ErrorBoundary>
+  );
+}
+
+function Resolved({ resource }: any) {
+  const data = resource.read();
+
   return (
     <section
       style={{ display: 'flex', flexWrap: 'wrap', rowGap: '24px', justifyContent: 'space-around' }}
     >
-      {loading ? (
-        <p>로딩중 ...</p>
-      ) : (
-        <>
-          {data &&
-            data?.map((item, idx) => (
-              <div key={item?.href}>
-                <EachCard
-                  imageUrl={item?.links && item?.links[0]?.href}
-                  title={item?.data[0]?.title}
-                  description={item?.data[0]?.description}
-                  dateCreated={item?.data[0]?.date_created}
-                  center={item?.data[0]?.center}
-                />
-              </div>
-            ))}
-          {error && <p>에러 났어요 {error.message}</p>}
-        </>
-      )}
+      {data?.collection?.items.map((item: any) => (
+        <div key={item?.href}>
+          <EachCard
+            imageUrl={item?.links && item?.links[0]?.href}
+            title={item?.data[0]?.title}
+            description={item?.data[0]?.description}
+            dateCreated={item?.data[0]?.date_created}
+            center={item?.data[0]?.center}
+          />
+        </div>
+      ))}
     </section>
   );
 }
--- example/src/lib/api.ts
+++ example/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
+import wrapPromise from '../utils/wrapPromise';
 
-// const BASE_URL = 'https://server.sodam.me';
 const BASE_URL = 'https://images-api.nasa.gov';
 
 const client = axios.create({
@@ -8,8 +8,11 @@ const client = axios.create({
   timeout: 5000,
 });
 
-// https://images-api.nasa.gov/search?q=earth
 export const getNasaData = async (keyword: string, page: number) => {
   const data = await client.get(`/search?q=${keyword}&page=${page}`);
   return data?.data;
 };
+
+export function fetchNasaData(keyword: string, page: number) {
+  return wrapPromise(getNasaData(keyword, page));
+}
